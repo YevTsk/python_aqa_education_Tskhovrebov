7a41e49b393e470ffe7cfe2182f3795dd4783a0e
Yevgenii Tskhovrebov <yevgenii.tsk@gmail.com>
2021-10-19 10:32:16 +0300

    create files for Python basics
    files for Python basics=>add counts
    
    delete idea folder

---
+++ python_basics/2_variables_and_types.py
@@ -0,0 +1,7 @@
+mystring = "my new string"
+myfloat = 5.01
+myint = 7
+
+print(mystring)
+print(myfloat)
+print(myint)
---
+++ python_basics/3_List.py
@@ -0,0 +1,17 @@
+numbers = []
+strings = []
+names = ["John", "Eric", "Jessica"]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("hello")
+strings.append("world")
+second_name = names[2]
+
+print(numbers)
+print(strings)
+print(f'The second name on the names list is {second_name}')
+
+
